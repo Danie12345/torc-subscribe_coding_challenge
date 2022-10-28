e547b642130a7dd80340c7a2f1b417a4b711a5ff
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-10-28 16:33:20 -0500

    Fix eslint errors and warnings

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import Order from "./modules/order.js";
-import { items1, items2, items3 } from "./modules/item_orders.js";
+import Order from './modules/order.js';
+import { items1, items2, items3 } from './modules/item_orders.js';
 
-function order_items(items) {
-    const order = new Order();
+function orderItems(items) {
+  const order = new Order();
 
-    items.forEach((item) => {
-        order.add_item(item);
-    });
+  items.forEach((item) => {
+    order.addItem(item);
+  });
 
-    console.log(order.repr());
-    console.log('');
+  console.log(order.repr());
+  console.log('');
 }
 
 function main() {
-    order_items(items1);
-    order_items(items2);
-    order_items(items3);
+  orderItems(items1);
+  orderItems(items2);
+  orderItems(items3);
 }
 
 main();
--- src/modules/items.js
+++ src/modules/items.js
@@ -1,46 +1,50 @@
 export default class Item {
-    constructor(description) {
-        const { name, imported, basic_applies, quantity, raw_price } = this.parse_description(description);
-        this.name = name;
-        this.imported = imported;
-        this.basic_applies = basic_applies;
-        this.quantity = quantity;
-        this.taxes = this.calculate_taxes(raw_price);
-        this.total = (raw_price + this.taxes).toFixed(2);
-    }
+  constructor(description) {
+    const {
+      name, imported, basicApplies, quantity, rawPrice,
+    } = this.parseDescription(description);
+    this.name = name;
+    this.imported = imported;
+    this.basicApplies = basicApplies;
+    this.quantity = quantity;
+    this.taxes = this.calculateTaxes(rawPrice);
+    this.total = (rawPrice + this.taxes).toFixed(2);
+  }
 
-    repr() {
-        return `${this.quantity} ${this.name}: ${this.total}`;
-    }
+  repr() {
+    return `${this.quantity} ${this.name}: ${this.total}`;
+  }
 
-    parse_description(description) {
-        const [qtydesc, price] = description.split(' at ');
-        const imported = 0 || 1*qtydesc.includes('imported');
-        let [qty, , ...name] = qtydesc.split('');
-        name = name.join('');
-        const basic_applies = this.determine_type(name);
-        const quantity = parseInt(qty);
-        const raw_price = quantity * price;
-        return { name, imported, basic_applies, quantity, raw_price };
-    }
+  parseDescription(description) {
+    const [qtydesc, price] = description.split(' at ');
+    const imported = 0 || 1 * qtydesc.includes('imported');
+    let [qty, , ...name] = qtydesc.split('');
+    name = name.join('');
+    const basicApplies = this.determineType(name);
+    qty = parseInt(qty, 10);
+    const rawPrice = qty * price;
+    return {
+      name, imported, basicApplies, qty, rawPrice,
+    };
+  }
 
-    determine_type(name) {
-        const no_basic_tax_products = [
-            'chocolate', 'bar', 'apple', 'sandwich', 'food', 'drink',
-            'pills', 'band', 'aid', 'syringe', 'medicine',
-            'book', 'notebook'
-        ];
-        return 1 - no_basic_tax_products.some((product) => name.includes(product));
-    }
+  determineType(name) {
+    const noBasicTaxProducts = [
+      'chocolate', 'bar', 'apple', 'sandwich', 'food', 'drink',
+      'pills', 'band', 'aid', 'syringe', 'medicine',
+      'book', 'notebook',
+    ];
+    return 1 - noBasicTaxProducts.some((product) => name.includes(product));
+  }
 
-    calculate_taxes(raw_price) {
-        const basic_tax = .1;
-        const import_tax = .05;
-        const total_tax = basic_tax*this.basic_applies + import_tax*this.imported;
-        const precision = .05;
-        const rounding = 1.0 / precision;
-        const total = raw_price * total_tax;
-        const corrected_total = Math.round(total * 100) / 100; // corrects bad binary to decimal rounding efforts
-        return Math.ceil(corrected_total * rounding) / rounding;
-    }
+  calculateTaxes(rawPrice) {
+    const basicTax = 0.1;
+    const importTax = 0.05;
+    const totalTax = basicTax * this.basicApplies + importTax * this.imported;
+    const precision = 0.05;
+    const rounding = 1.0 / precision;
+    const total = rawPrice * totalTax;
+    const correctedTotal = Math.round(total * 100) / 100; // corrects binary to decimal rounding
+    return Math.ceil(correctedTotal * rounding) / rounding;
+  }
 }
