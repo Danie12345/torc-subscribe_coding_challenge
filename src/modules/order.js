17ecb7cb0ba5c34b656e638cebd0e6c0659fbef3
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-10-28 16:17:05 -0500

    Add add item method to order class

--- src/modules/order.js
+++ src/modules/order.js
@@ -6,4 +6,11 @@ export default class Order {
         this.taxes = [];
         this.totals = [];
     }
+
+    add_item(description) {
+        const item = new Item(description);
+        this.taxes.push(item.taxes);
+        this.totals.push(item.total);
+        this.items.push(item);
+    }
 }
